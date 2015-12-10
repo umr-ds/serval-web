acee7728ce079865e211330356b203b320fca021
Stefan Schulz <schulzs@students.uni-marburg.de>
2015-12-10 12:32:36 +0900

    File alerts now automatically fade out, added responsive behavior to navbar

--- static/js/bootstrap_custom.js
+++ static/js/bootstrap_custom.js
@@ -1,6 +1,14 @@
-function create_alert(message) {
-    return  '<div class="alert alert-info alert-dismissible" role="alert">' +
+function create_alert(id, message) {
+    return  '<div id="' + id + '" class="alert alert-info alert-dismissible fade" role="alert">' +
                 '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>' +
                 message +
             '</div>';
+}
+
+function createAutoClosingAlert(id, message, parent, delay) {
+    var time_id = id + Date.now()
+    $(parent).append(create_alert(time_id, message));
+    var alert = $("#" + time_id);
+    $(".fade").addClass("in")
+    window.setTimeout(function() {alert.alert('close')}, delay);
 }
